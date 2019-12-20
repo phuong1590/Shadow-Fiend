b0733e37d1c60bdc7b6e2e7e8710545310ddf17d
phuong1590 <56270312+phuong1590@users.noreply.github.com>
2019-12-20 14:38:12 +0700

    Made a main function

--- main.cpp
+++ main.cpp
@@ -1,6 +1,10 @@
 #include <iostream>;
 
 using namespace std;
+int main{
+  cout << "Welcome,this program will help you calculate SF's third ability additional damage." << endl;
+  cout << "To those who don't know anthing about Shadow Fiend or Dota 2 in the first place,don't worry,I will walk you right through it" << endl;
+  return 0;
+}
+  
 
-cout << "Welcome,this program will help you calculate SF's third ability additional damage." << endl;
-cout << "To those who don't know anything about Shadow Fiend or Dota 2 in the first place,don't worry,I will walk you right through it" << endl;
