12537c6a9aeeede708e43188588f94ea3bf7cfe1
phuong1590 <56270312+phuong1590@users.noreply.github.com>
2019-12-20 14:35:46 +0700

    Created main.cpp
    A program that calculate SF's damage and additional damage after killing some creeps.And then,bases on that information,we can find out how many hits will it take in order to clear out the jungle.There are many aspect,because Dota 2 heroes have the damage between 2 numbers.For example,a maxed levelled SF will have around 136-142,according to Wiki,and the result also depends on the scenario,how many camps we have,is there any large or medium camps?I am just a students so I'm gonna skip all of that and take the scenario where SF always deals 136 damage(non additional damage from 3rd ability yet) and we have 4 small camps,each have 3 creeps and all of them have 350 hp.

---
+++ main.cpp
@@ -0,0 +1,6 @@
+#include <iostream>;
+
+using namespace std;
+
+cout << "Welcome,this program will help you calculate SF's third ability additional damage." << endl;
+cout << "To those who don't know anything about Shadow Fiend or Dota 2 in the first place,don't worry,I will walk you right through it" << endl;
